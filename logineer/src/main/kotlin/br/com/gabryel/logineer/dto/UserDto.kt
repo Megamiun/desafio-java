ad4a249cd37083845e73779148d2eed63386753e
Gabryel Monteiro <gabryel.monteiro@aditusbr.com>
2018-05-28 13:45:15 -0300

    Adding email validation

--- logineer/src/main/kotlin/br/com/gabryel/logineer/dto/UserDto.kt
+++ logineer/src/main/kotlin/br/com/gabryel/logineer/dto/UserDto.kt
@@ -1,12 +1,13 @@
 package br.com.gabryel.logineer.dto
 
 import com.fasterxml.jackson.annotation.JsonInclude
+import javax.validation.constraints.Email
 import javax.validation.constraints.NotBlank
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 data class UserDto @JvmOverloads constructor(
     @field:NotBlank var name: String? = null,
-    @field:NotBlank var email: String? = null,
+    @field:NotBlank @field:Email var email: String? = null,
     @field:NotBlank var password: String? = null,
     var phones: List<PhoneDto> = listOf()
 )
